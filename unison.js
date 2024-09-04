0ebca0ad98c6edf45b49c5602dac83361d3ad79e
Jeremy <jabulencia@yondu.com>
2024-09-04 16:36:42 +0800

    Initial Commit

---
+++ unison.js
@@ -0,0 +1,56 @@
+(function($) {
+    $.fn.mergeElements = function (options) {
+        var settings = $.extend({
+            mergePoint: $(this).width()*.5
+        },options);
+
+        return this.each(function() {
+            var $containerElement = $(this);
+            var $containerWidth = $containerElement.width();
+            var $containerHalfWidth = settings.mergePoint;
+            var $leftElement = $(this).find('.left-element');
+            var $rightElement = $(this).find('.right-element');
+            var $leftPosition = 0;
+            var $rightPosition = 0;
+            var $scrollTop = $(window).scrollTop();
+
+            $containerElement.css({
+                "position":"fixed",
+                "margin":"auto",
+                "width":"80%",
+                "display":"inline",
+            });
+            $leftElement.css({
+                "width":"auto",
+                "position":"absolute",
+                "left":$leftPosition,
+            })
+            $rightElement.css({
+                "width":"auto",
+                "position":"absolute",
+                "right":$rightPosition,
+            })
+
+            function positioning(scrollTopValue){
+                if (scrollTopValue <= $containerHalfWidth) {
+                    $leftElement.css("left",scrollTopValue);
+                    $rightElement.css("right",scrollTopValue);
+                } else {
+                    $leftElement.css("left",$containerHalfWidth);
+                    $rightElement.css("right",$containerHalfWidth);
+                }            
+            }
+
+            positioning($scrollTop);
+
+            $(window).scroll(function () {
+                $scrollTop = $(window).scrollTop();
+                $leftPosition = $scrollTop;
+                $rightPosition = $scrollTop;
+                
+                positioning($scrollTop);
+            });
+
+        });
+    }
+})(jQuery);
